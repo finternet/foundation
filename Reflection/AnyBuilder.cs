e7deffcf9dc559648aa352228213acbfb78e0e3f
Mohammad J. Fathinia <mj.fathinia@gmail.com>
2015-07-20 12:19:59 +0400

    Added Reflection helpers

---
+++ Reflection/AnyBuilder.cs
@@ -0,0 +1,27 @@
+﻿namespace FI.Foundation.Reflection
+{
+    public class AnyBuilder
+    {
+        public static object Create(string typeName)
+        {
+            string assemblyName = null;
+            if (typeName.IndexOf(',') >=0)
+            {
+                assemblyName = typeName.Substring(typeName.IndexOf(',') + 1);
+                typeName = typeName.Substring(0, typeName.IndexOf(','));
+            }
+
+            System.Reflection.Assembly assembly = null;
+            if (string.IsNullOrWhiteSpace(assemblyName))
+                assembly = System.Reflection.Assembly.GetCallingAssembly();
+            else
+                assembly = System.Reflection.Assembly.Load(assemblyName);
+
+            if (assembly != null)
+            {
+                return assembly.CreateInstance(typeName);
+            }
+            return null;
+        }
+    }
+}
---
+++ Reflection/PropertyHelper.cs
@@ -0,0 +1,38 @@
+﻿using FI.Foundation.Dynamic;
+using System;
+using System.Reflection;
+
+namespace FI.Foundation.Reflection
+{
+    public class PropertyHelper
+    {
+        public static void SetPropertyValue(object obj, string propertyName, object propertyValue)
+        {
+            SetPropertyValue(obj, obj.GetType(), obj.GetType().GetProperty(propertyName), propertyValue);
+        }
+        public static void SetPropertyValue(object obj, Type type, string propertyName, object propertyValue)
+        {
+            SetPropertyValue(obj, type, type.GetProperty(propertyName), propertyValue);
+        }
+        public static void SetPropertyValue(object obj, Type type, PropertyInfo property, object propertyValue)
+        {
+            SetHandler setPropertyHandler = DynamicMethodCompiler.CreateSetHandler(type,
+                    property);
+            setPropertyHandler(obj, propertyValue);
+        }
+        public static object GetPropertyValue(object obj, string propertyName)
+        {
+            return GetPropertyValue(obj, obj.GetType(), obj.GetType().GetProperty(propertyName));
+        }
+        public static object GetPropertyValue(object obj, Type type, string propertyName)
+        {
+            return GetPropertyValue(obj, type, type.GetProperty(propertyName));
+        }
+        public static object GetPropertyValue(object obj, Type type, PropertyInfo property)
+        {
+            GetHandler getPropertyHandler = DynamicMethodCompiler.CreateGetHandler(
+                type, property);
+            return getPropertyHandler(obj);
+        }
+    }
+}
