9663b4a6549c162a901398f3f18bb63d92cb83e8
Mohammad J. Fathinia <mj.fathinia@gmail.com>
2015-07-21 13:29:27 +0400

    Added threading and timer helpers

---
+++ Threading/TimerHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FI.Foundation.Threading
+{
+    public static class TimerHelpers
+    {
+        public static long Elapsed(Action action)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            action.Invoke();
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
